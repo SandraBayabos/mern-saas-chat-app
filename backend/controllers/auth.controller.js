adc23ebed8dfd001d357eca3da55c7dc59af54a3
sandrabayabos <sandra.bayabos@gmail.com>
2024-03-14 10:59:56 +0800

    fixed server.js dotenv imports, added gender to req.body

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,47 @@
+import User from "../models/user.model.js";
+
 export const signup = async (req, res) => {
   try {
-    const { fullName, username, password, confirmPassword } = req.body;
-  } catch (error) {}
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ message: "Password do not match" });
+    }
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // HASH PASSWORD HERE
+
+    // https://avatar-placeholder.iran.liara.run
+    const boyProfilePicture = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePicture = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      password,
+      gender,
+      profilePicture:
+        gender === "male" ? boyProfilePicture : girlProfilePicture,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({
+      _id: newUser._id,
+      fullName: newUser.fullName,
+      username: newUser.fullName,
+      profilePicture: newUser.profilePicture,
+      message: "User created successfully",
+    });
+  } catch (error) {
+    console.log("Error in signup", error.message);
+    res.status(500).json({ message: `${error.message}` });
+  }
 };
 
 export const login = (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,12 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/conntectToMongoDB.js";
 
-const app = express();
-
 dotenv.config();
 
+const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!!");
-});
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 
 app.use("/api/auth", authRoutes);
 
